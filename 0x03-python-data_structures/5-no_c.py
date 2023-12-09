4239f4402e3a65c2c745b06ba546d699f42bfe37
Bilal <taybib4@gmail.com>
2023-12-09 22:42:08 +0100

    no c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    result = ""
+    i = 0
+    while i < len(my_string):
+        if my_string[i] not in ('c', 'C'):
+            result += my_string[i]
+        i += 1
+    return (result)
